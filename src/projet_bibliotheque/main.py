8929f2ab14599bbf2b5cbb6e601946723ed47d84
Nisco <dehloumyounes03@gmail.com>
2025-12-23 19:13:58 +0100

    qsdfsdf sdf dfs

--- src/projet_bibliotheque/main.py
+++ src/projet_bibliotheque/main.py
@@ -13,7 +13,17 @@ while True:
 
     choix = input ("Tapez le numéro de votre choix")
     
-        
+    if choix == "1" :
+        isbn = input("entrez isbn")
+        titre = input("entrez titre")
+        auteur = input("entrez auteur")
+        categorie = input("entrez la catégorie")
+        etat = input("le livre est il disponible? o pour oui n pour non")
+
+        etat == True if etat == "o" else False
+        nouveau_livre = Livre(isbn, titre, auteur, categorie, etat)
+
+        ma_bibliotheque.ajouter_livre(nouveau_livre)
         
     if choix == "2":
         isbn = input("entrez isbn")
